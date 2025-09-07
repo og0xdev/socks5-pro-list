fd1609fde2d21e50440e6ba3fa8a4db7f8f44289
og0xdev <szabofilmek2@gmail.com>
2025-09-07 08:55:02 +0000

    Auto Update 2025-09-07 08:55:01

--- commit.sh
+++ commit.sh
@@ -1,4 +1,12 @@
-git checkout main
-git add .
-git commit -m "Auto Update $(date '+%Y-%m-%d %H:%M:%S')" || echo "Nothing to commit"
-git push origin main --force
+while true
+do
+   git checkout main
+   git add .
+
+   git diff --cached --quiet || \
+   git commit -m "Auto Update $(date '+%Y-%m-%d %H:%M:%S')" || echo "Nothing to commit"
+   
+   git push origin main --force
+
+   sleep $((300 + RANDOM % 300))
+done
